a9dcc688facca297ffcaf377ab8b63f6815a8450
Will Richards <richardsw2017@gmail.com>
2019-01-29 11:40:39 -0700

    added jellyfish as CMake ExternalProject

---
+++ externals/jellyfish.cmake
@@ -0,0 +1,23 @@
+SET_PROPERTY(DIRECTORY PROPERTY "EP_BASE" ${ep_base})
+
+SET(JELLYFISH_PROJECT jellyfish_project CACHE INTERNAL "jellyfish project name")
+SET(JELLYFISH_DIR ${CMAKE_BINARY_DIR}/externals/jellyfish CACHE INTERNAL "jellyfish project directory")
+SET(JELLYFISH_LIB)
+
+
+
+ExternalProject_Add(${JELLYFISH_PROJECT}
+	URL https://github.com/gmarcais/Jellyfish/releases/download/v1.1.12/jellyfish-1.1.12.tar.gz
+	CONFIGURE_COMMAND ${PROJECT_SOURCE_DIR}/bin/externals/jellyfish/src/jellyfish_project/configure --prefix=${PROJECT_SOURCE_DIR}/bin/externals/jellyfish/src/jellyfish_project/
+	BUILD_IN_SOURCE 1
+	BUILD_COMMAND make
+	INSTALL_COMMAND make install
+	UPDATE_COMMAND ""
+	PREFIX ${JELLYFISH_DIR}
+)
+
+ExternalProject_Get_Property(${JELLYFISH_PROJECT} INSTALL_DIR)
+ExternalProject_Get_Property(${JELLYFISH_PROJECT} SOURCE_DIR)
+ExternalProject_Get_Property(${JELLYFISH_PROJECT} BINARY_DIR)
+
+SET(JELLYFISH_INCLUDE ${SOURCE_DIR} CACHE INTERNAL "JELLYFISH INCLUDE")
