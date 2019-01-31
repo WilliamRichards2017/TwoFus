3cd8d502d291796541cf89dd09c35bc05a46f7a0
Will Richards <richardsw2017@gmail.com>
2019-01-30 20:45:59 -0700

    updated runScript to verify if run successfull in RUFUS test

--- tools/runTwofus.cpp
+++ tools/runTwofus.cpp
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <list>
 #include <stdio.h>
@@ -10,7 +11,13 @@
 
 int main(int argc, char * argv[] ){
 
+  std::cout << "Running TwoFus" << std::endl;
+
   input i = {argc, argv};
   contigs c = {i};
+
+  std::cout << "Finished TwoFus with exit code 0" << std::endl;
+
+  return 0;
 
 }
